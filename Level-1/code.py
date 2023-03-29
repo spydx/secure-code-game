f2e76518e944506618c2729ed20b1fb9b2255043
Kenneth Fossen <fossen.kenneth@gmail.com>
2023-03-29 11:39:26 +0200

    Level 1

--- Level-1/code.py
+++ Level-1/code.py
@@ -16,14 +16,22 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+MAX_ITEM_AMOUNT = 100000
+MAX_QUANTITY = 100
+MAX_TOTAL = 1e6
+
 def validorder(order: Order):
     net = 0
     
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if item.amount > -1*MAX_ITEM_AMOUNT and item.amount < MAX_ITEM_AMOUNT:
+                net += item.amount
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if item.quantity > 0 and item.quantity <= MAX_QUANTITY and item.amount > 0 and item.amount <= MAX_ITEM_AMOUNT:
+                net -= item.amount * item.quantity
+            if net > MAX_TOTAL or net < -1*MAX_TOTAL:
+                return("Total amount exceeded")
         else:
             return("Invalid item type: %s" % item.type)
     
